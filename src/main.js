c14806ad9b10450ecf4a41a565d4cabb368f7a1b
ashleyrevlett <ashleyrevlett@gmail.com>
2016-12-18 00:20:48 -0500

    Rule 1 for flocking algo done

--- src/main.js
+++ src/main.js
@@ -1,3 +1,31 @@
+class Vector {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+	}
+	magnitude() {
+		return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+	}
+	static normalize (vector) {
+		let mag = vector.magnitude();
+		let newX = vector.x / mag;
+		let newY = vector.y / mag;
+		return new Vector(newX, newY);
+  	}
+	static add (vector1, vector2) {
+		let newX = vector1.x + vector2.x;
+		let newY = vector1.y + vector2.y;
+		return new Vector(newX, newY);
+  	}
+	static subtract (vector1, vector2) {
+		// vector1 - vector2
+		let newX = vector1.x - vector2.x;
+		let newY = vector1.y - vector2.y;
+		return new Vector(newX, newY);
+  	}
+}
+
+
 class Boid {
 	constructor(context, xpos, ypos, radius) {
 		this.context = context;
@@ -5,9 +33,9 @@ class Boid {
 		this.y = ypos;
 		this.radius = radius;
 	}
-	update() {
+	update(center) {
+		this.move(center);
 		this.draw();
-		this.updatePosition();
 	}
 	draw() {
 		this.context.beginPath();
@@ -16,9 +44,28 @@ class Boid {
 		this.context.fill();
 		this.context.closePath();
 	}
-	updatePosition() {
-		this.x += 1;
-		this.y += 1;
+	move(center) {
+		let newPos = this.rule1(center);
+		this.x = newPos.x;
+		this.y = newPos.y;
+	}
+	rule1(center) {
+		// move 1% toward center point
+		// get vector from current position toward center point
+		let currentPos = new Vector(this.x, this.y);
+		let moveDir = Vector.subtract(center, currentPos);
+		moveDir = Vector.normalize(moveDir);
+		let newPos = Vector.add(currentPos, moveDir);
+
+		// debug draw
+		// this.context.beginPath();
+		// this.context.lineWidth="5";
+		// this.context.strokeStyle="#009000"; // Green path
+		// this.context.moveTo(currentPos.x,currentPos.y);
+		// this.context.lineTo(newPos.x,newPos.y);
+		// this.context.stroke(); // Draw it
+
+		return newPos;
 	}
 }
 
@@ -30,10 +77,12 @@ class Flock {
 		this.radius = 10;
 		this.canvas_width = this.cvs.width;
 		this.canvas_height = this.cvs.height;
+		this.speed = 10;
 		this.flock = this.createFlock(); // array of Boid objects
+		this.center_point = new Vector(0, 0);
 		window.setInterval(function(){
 			this.update();
-		}.bind(this), 60)
+		}.bind(this), 60); // have to bind 'this' to interval scope
 	}
 	createFlock(flock) {
 		let boids = new Array();
@@ -46,15 +95,30 @@ class Flock {
 		return boids;
 	}
 	update() {
-		try {
-		    this.ctx.clearRect(0, 0, this.canvas_width, this.canvas_height);
-			this.flock.forEach(f => f.update());
+		this.ctx.clearRect(0, 0, this.canvas_width, this.canvas_height);
+		this.center_point = this.calculateCenter();
+		this.drawCenter(this.center_point.x, this.center_point.y);
+		for (let i = 0; i < this.totalBoids; i++) {
+			this.flock[i].update(this.center_point);
 		}
-		catch (e) {
-		    // statements to handle any exceptions
-			console.log("ERROR");
-			console.log(e);
+	}
+	calculateCenter() {
+		let average_x = 0;
+		let average_y = 0;
+		for (let i = 0; i < this.totalBoids; i++) {
+			average_x += this.flock[i].x;
+			average_y += this.flock[i].y;
 		}
+		average_x /= this.totalBoids;
+		average_y /= this.totalBoids;
+		return new Vector(average_x, average_y);
+	}
+	drawCenter(x, y) {
+		this.ctx.beginPath();
+		this.ctx.arc(x, y, 20, 0, Math.PI * 2);
+		this.ctx.fillStyle = "#ffff00";
+		this.ctx.fill();
+		this.ctx.closePath();
 	}
 }
 
